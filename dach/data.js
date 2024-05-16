e822ef71ac26f9749e6d43edb17699f622f7d36b
Amt-Biesenthal-Barnim <148776656+Amt-Biesenthal-Barnim@users.noreply.github.com>
2024-05-16 15:55:13 +0200

    Add files via upload

---
+++ dach/data.js
@@ -0,0 +1,672 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dach-01",
+      "name": "Dach 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.404390875400014,
+        "pitch": 0.29493819845879443,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10950622291095691,
+          "pitch": 0.15237742804242593,
+          "rotation": 0,
+          "target": "12-garten_01"
+        },
+        {
+          "yaw": 2.0952113371566545,
+          "pitch": 0.22028742345338514,
+          "rotation": 0,
+          "target": "1-dach-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dach-02",
+      "name": "Dach 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.16899219741531724,
+        "pitch": 0.13976610093601494,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05176454918735729,
+          "pitch": 0.12516465774443652,
+          "rotation": 0,
+          "target": "2-dach-03"
+        },
+        {
+          "yaw": -1.7617074654229583,
+          "pitch": 0.24130254412100527,
+          "rotation": 0,
+          "target": "0-dach-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dach-03",
+      "name": "Dach 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.01060330829431777,
+        "pitch": 0.08219437360417459,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1253422677543057,
+          "pitch": 0.09426444163346659,
+          "rotation": 0,
+          "target": "1-dach-02"
+        },
+        {
+          "yaw": -1.5196565528822354,
+          "pitch": 0.24101003508888397,
+          "rotation": 0,
+          "target": "3-dach-04"
+        },
+        {
+          "yaw": 0.060255714928114656,
+          "pitch": 0.1675693735722703,
+          "rotation": 0,
+          "target": "4-dach-05"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dach-04",
+      "name": "Dach 04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.08306320188530947,
+        "pitch": 0.2722702778605246,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4964372012123217,
+          "pitch": 0.20412603881973013,
+          "rotation": 0,
+          "target": "2-dach-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-dach-05",
+      "name": "Dach 05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.4899319282016172,
+        "pitch": 0.034125472080926755,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.638111452941411,
+          "pitch": 0.12692465845022127,
+          "rotation": 0,
+          "target": "2-dach-03"
+        },
+        {
+          "yaw": -0.009734062079242989,
+          "pitch": 0.35053290565460316,
+          "rotation": 0,
+          "target": "5-dach-06"
+        },
+        {
+          "yaw": 1.5584145984424396,
+          "pitch": 0.09050099980484205,
+          "rotation": 0,
+          "target": "6-dach-07"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dach-06",
+      "name": "Dach 06",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.09973310011396208,
+        "pitch": 0.26138076019200085,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5557013208897317,
+          "pitch": 0.3513880690761013,
+          "rotation": 0,
+          "target": "4-dach-05"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.870320493913562,
+          "pitch": -0.044620294037443387,
+          "title": "Swegon Global PX",
+          "text": "Lüftungsgerät mit Gegenstromwärmetauscher"
+        }
+      ]
+    },
+    {
+      "id": "6-dach-07",
+      "name": "Dach 07",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.505596181116676,
+        "pitch": 0.21600215599201178,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5948513079953663,
+          "pitch": 0.10114101819366716,
+          "rotation": 0,
+          "target": "4-dach-05"
+        },
+        {
+          "yaw": 1.7951952532183224,
+          "pitch": 0.1884923980453781,
+          "rotation": 0,
+          "target": "7-dach-08"
+        },
+        {
+          "yaw": 0.5168203875331816,
+          "pitch": 0.12768038481785027,
+          "rotation": 0,
+          "target": "8-dach-09"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-dach-08",
+      "name": "Dach 08",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.924114716583439,
+        "pitch": 0.3394363271278671,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7524469392558153,
+          "pitch": 0.122965945218656,
+          "rotation": 0,
+          "target": "6-dach-07"
+        },
+        {
+          "yaw": -2.0821357676401853,
+          "pitch": 0.1376500761088444,
+          "rotation": 0,
+          "target": "8-dach-09"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-dach-09",
+      "name": "Dach 09",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.877313558319184,
+        "pitch": 0.30472624521233627,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0546817987132329,
+          "pitch": 0.12318984643434483,
+          "rotation": 0,
+          "target": "7-dach-08"
+        },
+        {
+          "yaw": 2.6107811367836034,
+          "pitch": 0.20809217992881912,
+          "rotation": 0,
+          "target": "11-dach-12"
+        },
+        {
+          "yaw": -2.1539966604816847,
+          "pitch": 0.13681829477495988,
+          "rotation": 0,
+          "target": "9-dach-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-dach-10",
+      "name": "Dach 10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.9867170407848107,
+        "pitch": 0.34669253608801043,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.624724932981824,
+          "pitch": 0.15866256850459592,
+          "rotation": 0,
+          "target": "10-dach-11"
+        },
+        {
+          "yaw": 0.9887753158650305,
+          "pitch": 0.13025196007645867,
+          "rotation": 0,
+          "target": "8-dach-09"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5898229745260561,
+          "pitch": 0.04696542624336075,
+          "title": "Energie",
+          "text": "Photovoltaikanlagen"
+        }
+      ]
+    },
+    {
+      "id": "10-dach-11",
+      "name": "Dach 11",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.205007164646009,
+        "pitch": 0.3013139318880107,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.843004094379383,
+          "pitch": 0.1905188203871937,
+          "rotation": 0,
+          "target": "9-dach-10"
+        },
+        {
+          "yaw": -1.7123568552307233,
+          "pitch": 0.1259787140750639,
+          "rotation": 0,
+          "target": "11-dach-12"
+        },
+        {
+          "yaw": 0.28639386234507924,
+          "pitch": 0.288194431842447,
+          "rotation": 0,
+          "target": "13-garten_02"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.7404569725796275,
+          "pitch": 0.0037744594203914517,
+          "title": "Energie",
+          "text": "Photovoltaikanlagen"
+        }
+      ]
+    },
+    {
+      "id": "11-dach-12",
+      "name": "Dach 12",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.6454947868486975,
+        "pitch": 0.29768364355200916,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.488909467004424,
+          "pitch": 0.1275858576307165,
+          "rotation": 0,
+          "target": "10-dach-11"
+        },
+        {
+          "yaw": 2.7924982626964914,
+          "pitch": 0.1962273873018372,
+          "rotation": 0,
+          "target": "8-dach-09"
+        },
+        {
+          "yaw": -2.237271731223709,
+          "pitch": 0.13109080574505683,
+          "rotation": 0,
+          "target": "7-dach-08"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-garten_01",
+      "name": "Garten 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.263357884236207,
+        "pitch": 0.2352313542789286,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1445031017197351,
+          "pitch": 0.12308216248025161,
+          "rotation": 0,
+          "target": "13-garten_02"
+        },
+        {
+          "yaw": 1.272275450927225,
+          "pitch": -0.06728880410777371,
+          "rotation": 0,
+          "target": "0-dach-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-garten_02",
+      "name": "Garten 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.1811597006687613,
+        "pitch": 0.14960445238089548,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.461375528200648,
+          "pitch": -0.1766224184482752,
+          "rotation": 0,
+          "target": "10-dach-11"
+        },
+        {
+          "yaw": 2.4593329656637755,
+          "pitch": 0.14230388432308416,
+          "rotation": 0,
+          "target": "12-garten_01"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Kita Rüdnitz Dach",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
